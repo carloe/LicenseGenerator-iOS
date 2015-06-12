835d632f5c40c926012d0b5000ab927f271e8903
Carlo Eugster <carlo@relaun.ch>
2015-06-12 09:10:55 +0200

    accept excludes via arguments

--- credits.py
+++ credits.py
@@ -6,7 +6,7 @@
     them into a Settings.bundle friendly plist. Inspired by JosephH
     and Sean's comments on stackoverflow: http://stackoverflow.com/q/6428353
 
-    :usage: ./credits.py -d project/ -o project/Settings.bundle/Credits.plist
+    :usage: ./credits.py -s project/ -o project/Settings.bundle/Credits.plist
 
     :author: Carlo Eugster (http://carlo.io)
     :license: MIT, see LICENSE for more details.
@@ -17,47 +17,65 @@ import sys
 import plistlib
 import re
 import codecs
-import getopt
+from optparse import OptionParser
+from optparse import Option, OptionValueError
 from copy import deepcopy
 
-EXCLUDE = []
+VERSION = '0.3'
+PROG = os.path.basename(os.path.splitext(__file__)[0])
+DESCRIPTION = '''Recursively searches the input directory for 'LICENSE.*' files and compiles them into a Settings.bundle friendly plist. Inspired by JosephH and Sean's comments on stackoverflow: http://stackoverflow.com/q/6428353'''
+
+class MultipleOption(Option):
+    ACTIONS = Option.ACTIONS + ("extend",)
+    STORE_ACTIONS = Option.STORE_ACTIONS + ("extend",)
+    TYPED_ACTIONS = Option.TYPED_ACTIONS + ("extend",)
+    ALWAYS_TYPED_ACTIONS = Option.ALWAYS_TYPED_ACTIONS + ("extend",)
+
+    def take_action(self, action, dest, opt, value, values, parser):
+        if action == "extend":
+            values.ensure_value(dest, []).append(value)
+        else:
+            Option.take_action(self, action, dest, opt, value, values, parser)
+
 
 def main(argv):
-    outputfile = None
-    inputdir = None
-    try:
-        opts, args = getopt.getopt(argv,"hd:o:",["idir=","ofile="])
-    except getopt.GetoptError:
-        printHelp(1)
-    for opt, arg in opts:
-        if opt == '-h':
-            printHelp(0)
-        elif opt in ("-d", "--dir"):
-            inputdir = arg
-        elif opt in ("-o", "--out"):
-            outputfile = arg
+    parser = OptionParser(option_class=MultipleOption,
+                              usage='usage: %prog -s source_path -o output_plist -e [exclude_paths]',
+                              version='%s %s' % (PROG, VERSION),
+                              description=DESCRIPTION)
+    parser.add_option('-s', '--source', 
+                   type="string",
+                  dest='inputpath', 
+                  metavar='source_path', 
+                  help='source directory to search for licenses')
+    parser.add_option('-o', '--output-plist', 
+                   type="string",
+                  dest='outputfile', 
+                  metavar='output_plist', 
+                  help='path to the plist to be generated')
+    parser.add_option('-e', '--exclude', 
+                  action="extend", type="string",
+                  dest='excludes', 
+                  metavar='path1, ...', 
+                  help='comma seperated list of paths to be excluded')
+    if len(sys.argv) == 1:
+        parser.parse_args(['--help'])
 
-    if(outputfile == None or inputdir == None):
-        printHelp(2)
+    OPTIONS, args = parser.parse_args()
 
-    if(not outputfile.endswith('.plist')):
+    if(not os.path.isdir(OPTIONS.inputpath)):
+        print "Error: Invalid source path: %s" % OPTIONS.inputpath
+        sys.exit(2)
+
+    if(not OPTIONS.outputfile.endswith('.plist')):
         print "Error: Outputfile must end in .plist"
         sys.exit(2)
 
-    if(not os.path.isdir(inputdir)):
-        print "Error: Input directory does not exist."
-        sys.exit(2)
-
-    plist = plistFromDir(inputdir)
-    plistlib.writePlist(plist,outputfile)
+    plist = plistFromDir(OPTIONS.inputpath, OPTIONS.excludes)
+    plistlib.writePlist(plist,OPTIONS.outputfile)
     return 0
 
-def printHelp(code):
-    print 'Suntax:'
-    print '\tcredits.py -d <sourcepath> -o <outputfile>'
-    sys.exit(code)
-
-def plistFromDir(dir):
+def plistFromDir(dir, excludes):
     """
     Recursilvely search 'dir' to generates plist objects from LICENSE files.
     """
@@ -67,7 +85,7 @@ def plistFromDir(dir):
         for file in files:
             if file.startswith("LICENSE"):
                 plistPath = os.path.join(root, file)
-                if not excludePath(plistPath):
+                if not excludePath(plistPath, excludes):
                     license = plistFromFile(plistPath)
                     plist['PreferenceSpecifiers'].append(license)
     return plist
@@ -90,8 +108,10 @@ def plistFromFile(path):
     group['FooterText'] = rchop(body, " \n\n")
     return group
     
-def excludePath(path):
-    for pattern in EXCLUDE:
+def excludePath(path, excludes):
+    if excludes is None:
+        return False
+    for pattern in excludes:
         if(re.search(pattern, path, re.S) != None):
             return True
     return False
